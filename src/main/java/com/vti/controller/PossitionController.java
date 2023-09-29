1fc26162c2c5d8dce22480359646f25d19e92fcb
daonqvti <dao.nguyenquang@vti.com.vn>
2023-09-29 15:41:39 +0700

    20230929_CreatePosition_daonqvti_position

--- src/main/java/com/vti/controller/PossitionController.java
+++ src/main/java/com/vti/controller/PossitionController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +36,11 @@ public class PossitionController {
 //		}
 
 		return new ResponseEntity<>(dtos, HttpStatus.OK);
+	}
+
+	@PostMapping()
+	public ResponseEntity<?> createPosition() {
+		return new ResponseEntity<String>("Create successfully!", HttpStatus.CREATED);
 	}
 
 }
